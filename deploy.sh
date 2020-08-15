ab34e6984841994db626b44091bf22157896a51a
amirrezaask <raskarpour@gmail.com>
2020-08-15 14:54:11 +0430

    use job specific cluster

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,20 @@
 echo 'Deploying config map'
 oc process -f ConfigMap.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
 
-echo 'Deploying JobManager Service'
+echo 'Deploying JobManagerService'
 oc process -f JobManagerService.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
 
-echo 'Deploying JobManger Rest Service'
+echo 'Deploying JobMangerRestService'
 oc process -f JobManagerRestService.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
 
 echo 'Deploying TaskManagerQueryStatService'
 oc process -f TaskManagerQueryStatService.yml .yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
 
+echo 'Deploying JobManagerJob'
+oc process -f JobManagerJob.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+
+echo 'Deploying JobManagerJobDeployment'
+oc process -f JobManagerJobDeployment.yml .yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
 
 
 
