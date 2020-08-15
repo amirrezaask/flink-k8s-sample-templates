abc33aba580c7fa8388703a4b79df6825247028e
amirrezaask <raskarpour@gmail.com>
2020-08-15 15:20:06 +0430

    fix problems with flink templates

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
+env > .env
+
 echo 'Deploying config map'
-oc process -f ConfigMap.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f ConfigMap.yml | oc apply -f -
 
 echo 'Deploying JobManagerService'
-oc process -f JobManagerService.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f JobManagerService.yml | oc apply -f -
 
 echo 'Deploying JobMangerRestService'
-oc process -f JobManagerRestService.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f JobManagerRestService.yml | oc apply -f -
 
 echo 'Deploying TaskManagerQueryStatService'
-oc process -f TaskManagerQueryStatService.yml .yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f TaskManagerQueryStatService.yml | oc apply -f -
 
 echo 'Deploying JobManagerJob'
-oc process -f JobManagerJob.yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f JobManagerJob.yml | oc apply -f -
 
 echo 'Deploying JobManagerJobDeployment'
-oc process -f JobManagerJobDeployment.yml .yml --param-file=.env --param --ignore-unknown-parameters=true | oc apply -f -
+oc process -f JobManagerJobDeployment.yml | oc apply -f -
 
 
 
