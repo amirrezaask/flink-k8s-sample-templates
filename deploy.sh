9c32a03272877ba7b9f275f812b02ba7000ea698
amirrezaask <raskarpour@gmail.com>
2020-08-18 18:11:28 +0430

    more simplified templates

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,13 @@
 #! /bin/bash
 env > .env
 
-echo 'Deploying config map'
-oc process -f ConfigMap.yml | oc apply -f -
 
-echo 'Deploying JobManagerService'
-oc process -f JobManagerService.yml | oc apply -f -
+echo 'Defining Common Resources'
+oc process -f common.yml | oc apply -f -
 
-echo 'Deploying JobMangerRestService'
-oc process -f JobManagerRestService.yml | oc apply -f -
+echo 'Deploying Job Cluster'
+oc process -f job-cluster.yml | oc apply -f -
 
-echo 'Deploying TaskManagerQueryStatService'
-oc process -f TaskManagerQueryStatService.yml | oc apply -f -
-
-echo 'Deploying JobManagerJob'
-oc process -f JobManagerJob.yml | oc apply -f -
-
-echo 'Deploying JobManagerJobDeployment'
-oc process -f TaskManagerJobDeployment.yml | oc apply -f -
 
 
 
